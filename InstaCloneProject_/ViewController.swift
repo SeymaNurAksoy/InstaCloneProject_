161866d76d9577cba095e78cd19964fe000a0e28
seymaNur367 <aksoyseyma192@gmail.com>
2021-03-03 12:00:10 -0800

    Initial Commit

---
+++ InstaCloneProject_/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaCloneProject_
+//
+//  Created by Şeyma Nur on 3.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
